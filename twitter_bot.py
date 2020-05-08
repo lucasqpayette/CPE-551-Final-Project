01958cf20d9bf0d1b13e17cbc37a761236785e73
Lucas <lqpayette97@gmail.com>
2020-05-08 16:24:08 -0400

    added reply functionality

--- twitter_bot.py
+++ twitter_bot.py
@@ -9,13 +9,15 @@ api = tweepy.API(auth)
 
 public_tweets = api.mentions_timeline()
 last_tweet = public_tweets[0].text
+reply_id = public_tweets[0].id
 last_tweet = last_tweet.split(' ')
+api.update_status('hello my friend', in_reply_to_status_id = reply_id, auto_populate_reply_metadata = True)
+# print(public_tweets)
+# print(last_tweet[1]) 
 
-print(last_tweet[1])
-
-for tweet in last_tweet:
-    if tweet == 'hello':
-        print(1)
-    else:
-        print(2)
-    # print(tweet.text)
+# for tweet in last_tweet:
+#     if tweet == 'hello':
+#         print(1)
+#     else:
+#         print(2)
+#     # print(tweet.text)
