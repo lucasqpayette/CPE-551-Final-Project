3aa968372184f406844e71f4e7015de7f7c71e06
Maria <mlardas@stevens.edu>
2020-05-08 14:12:07 -0400

    finished testing git commit

--- twitter_bot.py
+++ twitter_bot.py
@@ -2,8 +2,6 @@ import tweepy
 import backend
 import password
 
-hi_maria = 1 
-
 auth = tweepy.OAuthHandler(password.consumer_key, password.consumer_secret)
 auth.set_access_token(password.access_token, password.access_token_secret)
 
