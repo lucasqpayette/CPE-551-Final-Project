9e422096f5a05cd451808129862f144ed1428f97
Lucas <lqpayette97@gmail.com>
2020-05-08 13:08:04 -0400

    added basic twitter bot

---
+++ twitter_bot.py
@@ -0,0 +1,16 @@
+import tweepy
+
+consumer_key = 'sdf'
+consumer_secret = 'sdf'
+access_token = 'sdf'
+access_token_secret = 'sdf'
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
