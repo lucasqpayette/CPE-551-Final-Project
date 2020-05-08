2d33a5f2f754b1f64856f302d4ce6d7ec1b6139c
Lucas <lqpayette97@gmail.com>
2020-05-08 13:26:28 -0400

    changed public tweets to pull users 20 last tweets

--- twitter_bot.py
+++ twitter_bot.py
@@ -12,6 +12,6 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
+public_tweets = api.user_timeline()
 for tweet in public_tweets:
     print(tweet.text)
